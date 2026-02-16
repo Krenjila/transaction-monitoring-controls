85079033f9ed6a73b2fe87c6ce095e1579063680
Krenjila Sharma <krenjila1sharma@gmail.com>
2026-02-16 15:29:01 -0500

    Create transactions table in schema

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -1,1 +1,13 @@
-
+CREATE TABLE transactions (
+    transaction_id INT PRIMARY KEY,
+    transaction_date DATE,
+    amount NUMERIC(12,2),
+    vendor_id INT,
+    vendor_name TEXT,
+    vendor_country TEXT,
+    department TEXT,
+    invoice_id TEXT,
+    payment_method TEXT,
+    approver_id TEXT,
+    approval_limit NUMERIC(12,2)
+);
